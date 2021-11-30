38702abe72ccc85c2bf83951ff16c71ce510f46d
Hazzari <free2mmm@gmail.com>
2021-11-30 23:05:54 +0300

    fix: flake8 message

--- src/routers.py
+++ src/routers.py
@@ -18,8 +18,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
     path('upload/', include('src.image_uploader.urls')),
     path('token/access/', TokenRefreshView.as_view(), name='token_get_access'),
     path('token/both/', TokenObtainPairView.as_view(),
